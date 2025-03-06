9c5aeab034287ebf8349348fb003e6b9f4fa6db9
ciaracagney <ccagney2@illinois.edu>
2025-03-06 15:18:10 -0500

    reverting hmac changes

--- design/ectf25_design/gen_secrets.py
+++ design/ectf25_design/gen_secrets.py
@@ -30,9 +30,6 @@ def gen_secrets(channels: list[int]) -> bytes:
 
     :returns: Contents of the secrets file
     """
-    # Generate a random hmac key
-    hmac_key = os.urandom(32).hex()
-
     # Generate a random AES key
     aes_key = os.urandom(16).hex()
     
@@ -45,7 +42,6 @@ def gen_secrets(channels: list[int]) -> bytes:
     
     # Create the secrets object with encryption keys
     secrets = {
-        "hmac_key": hmac_key,
         "aes_key": aes_key,
         "channel_keys": channel_keys,
     }
