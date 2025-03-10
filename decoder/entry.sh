aa64bdd74ec205820f8bdc4104c0e322353fb31f
cp-57 <109479938+cp-57@users.noreply.github.com>
2025-03-10 13:35:50 -0400

    add deployment folder to /decoder and edit entry.sh to reflect it

--- decoder/entry.sh
+++ decoder/entry.sh
@@ -1,7 +1,7 @@
 #! /bin/bash
-cd /deployment;
+cd deployment;
 python create_global_secrets.py;
-cd /decoder;
+cd ..;
 make release DECODER_ID=${DECODER_ID};
 rm ./inc/secrets.h;
 cp build/max78000.elf build/max78000.bin /out;
